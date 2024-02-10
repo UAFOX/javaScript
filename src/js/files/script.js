00dfa4400b8b3e3e93528a84ff5eb1be9c9896a4
Alexandr <example@gmail.com>
2024-02-10 20:59:01 +0200

    Первый Коммит для первого проэкта по Курсу обучения Ява Скрипт

---
+++ src/js/files/script.js
@@ -0,0 +1,52 @@
+// Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
+// import { isMobile } from "./functions.js";
+// import { formsModules } from "./forms/forms.js";
+
+
+
+// "use strict";
+// let boxBox = 10;
+// const box = ['box.jpg',  'box-2.jpg', 'box-3.jpg', 'box-4.jpg', [boxBox = 1]];
+// console.log(box[4]);
+
+
+let incr = 10,
+    decr = 10;
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+    console.log(2*4 == '8');
+
+    const isCheked = true,
+            isClose = false;
+            console.log(isCheked && isClose);
+            
+            console.log(isCheked || isClose);
+
+            console.log(!isCheked || isClose);
+
+            
+    
+    
+
+
+
+
+// const result = confirm("Шото Нада?")
+// console.log(result);
+
+// const answer = prompt("Сколько вам Лет?", "18");
+// console.log(answer);
+
+
+
+
+
+        // Создание Вопросов  и Ответов
+// const answers = [];
+// answers[0] = prompt('Name', '');
+// answers[1] = prompt('Age', '');
+// answers[2] = prompt('Sex', '');
+// console.log(typeof(answers));
