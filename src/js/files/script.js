39a94e7e71d9ab76bc72f641f10bf7209823afa7
Alexandr <example@gmail.com>
2024-02-12 12:28:15 +0200

    Функции и стрелочные функции

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -7,22 +7,22 @@
 // const box = ['box.jpg',  'box-2.jpg', 'box-3.jpg', 'box-4.jpg', [boxBox = 1]];
 // console.log(box[4]);
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
 
-console.log(5 % 2);
-console.log(2 * 4 == "8");
+// console.log(5 % 2);
+// console.log(2 * 4 == "8");
 
-const isCheked = true,
-    isClose = false;
-console.log(isCheked && isClose);
+// const isCheked = true,
+//     isClose = false;
+// console.log(isCheked && isClose);
 
-console.log(isCheked || isClose);
+// console.log(isCheked || isClose);
 
-console.log(!isCheked || isClose);
+// console.log(!isCheked || isClose);
 
 // const result = confirm("Шото Нада?")
 // console.log(result);
@@ -74,7 +74,7 @@ console.log(!isCheked || isClose);
 //         break;
 // }
 
-let num = 50;
+// let num = 50;
 
 // while (num <= 55) {
 //         console.log(num);
@@ -94,38 +94,106 @@ let num = 50;
 //         num++;
 // }
 
-const numbefOfFilms = +prompt("Сколько фильмов вы уже просмотрели", "");
-const personalMovieDB = {
-    count: numbefOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt("Один из последних просмотренных фильмов?", ""),
-        b = prompt("Какой был рейтинг этого фильма?", "");
 
-    if (a != null && b != null && a != "" && b != "" && a.langth < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log("done");
-    } else {
-        console.log("error");
-        i--;
-    }
+
+// вопросы
+// const numbefOfFilms = +prompt("Сколько фильмов вы уже просмотрели", "");
+// const personalMovieDB = {
+//     count: numbefOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt("Один из последних просмотренных фильмов?", ""),
+//         b = prompt("Какой был рейтинг этого фильма?", "");
+
+//     if (a != null && b != null && a != "" && b != "" && a.langth < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log("done");
+//     } else {
+//         console.log("error");
+//         i--;
+//     }
+// }
+
+// if (personalMovieDB.count < 10) {
+//         console.log('Очень мало фильмов');
+// } else if (personalMovieDB.count >= 10 &&  personalMovieDB.count < 30) {
+//         console.log('Мощно конечно');
+//  } else if (personalMovieDB.count >= 30) {
+//         console.log('Дохрена');
+//  } else {
+//         console.log('Шото не так');
+        
+//  }
+
+
+// console.log(personalMovieDB);
+
+
+let num = 20;
+
+
+function showFirstMassage (text) {
+        console.log(text);
+        let num = 10;
+        console.log(num);
 }
 
-if (personalMovieDB.count < 10) {
-        console.log('Очень мало фильмов');
-} else if (personalMovieDB.count >= 10 &&  personalMovieDB.count < 30) {
-        console.log('Мощно конечно');
- } else if (personalMovieDB.count >= 30) {
-        console.log('Дохрена');
- } else {
-        console.log('Шото не так');
+showFirstMassage("Hello World!");
+console.log(num);
+
+
+
+
+// function calc(a, b) {
+//         return (a + b);
+// }
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
+
+
+
+
+
+// function ret() {
+//         let num = 50;
+//         return num;
+// }
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+
+
+// const logger = function() {
+//         console.log('hi bro');
         
- }
+// };
+
+// logger();
 
 
-console.log(personalMovieDB);
+
+
+// const calc = (a, b) => {
+// console.log('1');
+// return a + b;
+// };
+
+
+
+
+
+
+
+
+
+
+
+
