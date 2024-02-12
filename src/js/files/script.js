437838b11a703afd2b7d9c959ab4d0a469a994c0
Alexandr <example@gmail.com>
2024-02-12 09:54:49 +0200

    Создание приложения. ничего не понял вообще, но очень интересно

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -51,3 +51,24 @@ let incr = 10,
 // answers[1] = prompt('Age', '');
 // answers[2] = prompt('Sex', '');
 // console.log(typeof(answers));
+
+
+const numbefOfFilms = +prompt('Сколько фильмов вы уже просмотрели', '');
+const personalMovieDB = {
+        count: numbefOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('Какой был рейтинг этого фильма?', ''),
+        c = prompt('Один из последних просмотренных фильмов?', ''),
+        d = prompt('Какой был рейтинг этого фильма?', '');
+
+        personalMovieDB.movies[a] = b;
+        personalMovieDB.movies[c] = d;
+        console.log(personalMovieDB);
+        
+
